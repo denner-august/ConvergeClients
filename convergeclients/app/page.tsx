e12fb4d86f2b6d9748f3d332d7650bfe7a89845c
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-05-30 21:22:33 -0300

    atualizando estrutura da pagina

--- convergeclients/app/page.tsx
+++ convergeclients/app/page.tsx
@@ -1,14 +1,26 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/Container/app-sidebar";
+import { ContainerBox } from "@/components/containerBox/containerBox";
+
+import ConteudoBox from "../styles/conteudoBox.module.scss";
+import ContainerSideBar from "../styles/sidebar.module.scss";
+import { ClientesBox } from "@/components/clientesBox/ClientesBox";
+
 export default function Home() {
   return (
-    <>
-      <SidebarProvider>
-        <AppSidebar />
-        <main>
-          <SidebarTrigger />
-        </main>
-      </SidebarProvider>
-    </>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className={ContainerSideBar.Container}>
+        <div className={ContainerSideBar.headSideBar}>
+          <SidebarTrigger size="lg" className={ContainerSideBar.Trigger} />
+          <p>Gestor de tráfego</p>
+        </div>
+
+        <div className={ConteudoBox.Container}>
+          <ContainerBox />
+          <ClientesBox />
+        </div>
+      </main>
+    </SidebarProvider>
   );
 }
