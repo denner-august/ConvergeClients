585e48c7f189ccfadaf91102e33a7c1a724aa137
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-04 16:53:34 -0300

    Update ClientesBox.tsx

--- convergeclients/components/clientesBox/ClientesBox.tsx
+++ convergeclients/components/clientesBox/ClientesBox.tsx
@@ -1,10 +1,14 @@
 import styles from "./Clientes.module.scss";
+import { ClientesInfo } from "./clientesInfo";
 
 export function ClientesBox() {
   return (
     <div className={styles.Container}>
       <input type="text" placeholder="Pesquisar cliente" />
       <p>Clientes cadastrados</p>
+      <div className={styles.ClientesInfo}>
+        <ClientesInfo />
+      </div>
     </div>
   );
 }
