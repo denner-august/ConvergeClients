0bf691b6bb0de5802ac92263c794f7de32f88417
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-11 18:27:52 -0300

    Update ClientesBox.tsx

--- convergeclients/components/clientesBox/ClientesBox.tsx
+++ convergeclients/components/clientesBox/ClientesBox.tsx
@@ -1,13 +1,46 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./Clientes.module.scss";
 import { ClientesInfo } from "./clientesInfo";
 
-export function ClientesBox() {
+interface ClientesProps {
+  id: number;
+  ClientNome: String;
+  titulo: string;
+  descrição: string;
+  modeloDeNegocio: string;
+  valorAtualDoContrato: string;
+  dataInicio: string;
+}
+
+export function ClientesBox({ AllClients }: { AllClients: ClientesProps[] }) {
+  const [searchClientes, setSearchClientes] = useState("");
+  const [clientAtual, setClientAtual] = useState(
+    (AllClients as ClientesProps[]) || []
+  );
+
+  useEffect(() => {
+    if (!searchClientes) {
+      setClientAtual(AllClients);
+    } else {
+      setClientAtual(
+        AllClients.filter((cliente) =>
+          cliente.titulo.toLowerCase().includes(searchClientes.toLowerCase())
+        )
+      );
+    }
+  }, [searchClientes, AllClients]);
+
   return (
     <div className={styles.Container}>
-      <input type="text" placeholder="Pesquisar cliente" />
+      <input
+        type="text"
+        placeholder="Pesquisar por clientes"
+        onChange={(e) => setSearchClientes(e.target.value)}
+      />
       <p>Clientes cadastrados</p>
       <div className={styles.ClientesInfo}>
-        <ClientesInfo />
+        <ClientesInfo clientes={clientAtual} />
       </div>
     </div>
   );
