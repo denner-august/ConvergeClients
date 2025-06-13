10f020f5589bdd7a0e6585ab7a0ddd6a4e416724
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-13 16:12:21 -0300

    Update route.ts

--- convergeclients/app/api/users/createUsers/route.ts
+++ convergeclients/app/api/users/createUsers/route.ts
@@ -1,9 +1,8 @@
-import type { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const user = await prisma.users.findFirst({
     where: {
       ClientNome: "denner",
