5f4b1812f037c229dccdb339ce84cccee4469ff3
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-13 20:51:56 -0300

    Update route.ts

--- convergeclients/app/api/users/createUsers/route.ts
+++ convergeclients/app/api/users/createUsers/route.ts
@@ -2,16 +2,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const user = await prisma.users.findFirst({
-    where: {
-      ClientNome: "denner",
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  const { titulo, name, descrição, modeloNegocio, valorContrato, dataInicio } =
+    data;
+
+  const create = await prisma.users.create({
+    data: {
+      titulo: titulo,
+      ClientNome: name,
+      descrição: descrição,
+      modeloDeNegocio: modeloNegocio,
+      dataInicio: new Date(dataInicio),
+      valorAtualDoContrato: Number(valorContrato),
     },
   });
 
-  if (user) {
-    return Response.json(user);
+  if (create) {
+    return Response.json({ message: "usuario criado" }, { status: 200 });
   }
 
-  return Response.json({ message: "usuario não encontrado" });
+  return Response.json({ message: "Erro ao criar cliente" }, { status: 400 });
 }
