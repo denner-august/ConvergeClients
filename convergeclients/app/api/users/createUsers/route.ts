e3da86df1faf222bc342a2a3ea331e89d7674d4b
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-15 18:17:25 -0300

    Update route.ts

--- convergeclients/app/api/users/createUsers/route.ts
+++ convergeclients/app/api/users/createUsers/route.ts
@@ -3,25 +3,39 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-  const data = await request.json();
+  try {
+    const data = await request.json();
 
-  const { titulo, name, descrição, modeloNegocio, valorContrato, dataInicio } =
-    data;
+    const {
+      titulo,
+      name,
+      descrição,
+      modeloNegocio,
+      valorContrato,
+      dataInicio,
+    } = data;
 
-  const create = await prisma.users.create({
-    data: {
-      titulo: titulo,
-      ClientNome: name,
-      descrição: descrição,
-      modeloDeNegocio: modeloNegocio,
-      dataInicio: new Date(dataInicio),
-      valorAtualDoContrato: Number(valorContrato),
-    },
-  });
+    const create = await prisma.users.create({
+      data: {
+        titulo: titulo,
+        ClientNome: name,
+        descrição: descrição,
+        modeloDeNegocio: modeloNegocio,
+        dataInicio:
+          isNaN(new Date(dataInicio).getTime()) === true
+            ? new Date()
+            : new Date(dataInicio),
+        valorAtualDoContrato: Number(valorContrato),
+      },
+    });
 
-  if (create) {
-    return Response.json({ message: "usuario criado" }, { status: 200 });
+    if (create) {
+      return Response.json({ message: "usuario criado" }, { status: 200 });
+    }
+  } catch (error) {
+    return Response.json(
+      { message: "Erro ao criar cliente", error: error.message },
+      { status: 500 }
+    );
   }
-
-  return Response.json({ message: "Erro ao criar cliente" }, { status: 400 });
 }
