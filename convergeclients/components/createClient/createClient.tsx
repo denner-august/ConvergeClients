2d5e7f63223722f0d544a36c3f8e7cd591d2f79f
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-18 17:24:54 -0300

    Update createClient.tsx

--- convergeclients/components/createClient/createClient.tsx
+++ convergeclients/components/createClient/createClient.tsx
@@ -1,11 +1,9 @@
 "use client";
-import styles from "./createClient.module.scss";
-import { Button } from "@/components/ui/button";
-import { DayPicker } from "react-day-picker";
 
+import styles from "./createClient.module.scss";
 import "react-day-picker/dist/style.css";
 
-import { apiCreateClient } from "./apiCreatClient";
+import { Button } from "@/components/ui/button";
 
 import {
   Dialog,
@@ -22,8 +20,44 @@ import { Label } from "@/components/ui/label";
 import { UserPlus } from "lucide-react";
 import { useState } from "react";
 
-export function DialogDemo() {
-  const [selected, setSelected] = useState<Date>();
+import { z } from "zod/v4";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { DayPicker } from "react-day-picker";
+import { apiCreateClient } from "./apiCreatClient";
+
+const schema = z.object({
+  titulo: z
+    .string({ error: "O titulo é obrigatório" })
+    .min(1, "O titulo é obrigatório"),
+  nome: z
+    .string({ error: "O nome é obrigatório" })
+    .min(1, "O nome é obrigatório"),
+  descrição: z
+    .string({ error: "A descrição é obrigatória" })
+    .min(1, "A descrição é obrigatória"),
+  modeloDeNegocio: z
+    .string({ error: "O modelo de negocio é obrigatório" })
+    .min(1, "O modelo de negocio é obrigatório"),
+  // O valor do contrato deve ser um número
+  valorDoContrato: z
+    .number({ error: "O valor do contrato deve ser positivo" })
+    .positive({ error: "O valor do contrato deve ser positivo" }),
+  inicioDoContrato: z.date().optional(),
+});
+
+export function FormCreate() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+  const [selected, setSelected] = useState<Date>(new Date());
+
+  function onSubmit(values: z.infer<typeof schema>) {
+    apiCreateClient(values, selected);
+  }
 
   return (
     <Dialog>
@@ -41,36 +75,39 @@ export function DialogDemo() {
             Preencha todas as abaixo para adicionar um novo cliente.
           </DialogDescription>
         </DialogHeader>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault(); // Evita o recarregamento da página
-            const formData = new FormData(e.currentTarget);
-            apiCreateClient(formData, selected);
-          }}
-        >
+
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label htmlFor="titulo">Titulo</Label>
+
+              <p>{errors.titulo?.message}</p>
               <Input
+                defaultValue={"titulo do cliente"}
                 id="titulo"
-                name="titulo"
                 placeholder="Escolha um titulo para o cliente"
+                {...register("titulo")}
               />
             </div>
+
             <div className="grid gap-3">
               <Label htmlFor="NomeCliente">Nome</Label>
+              <p>{errors.nome?.message}</p>
               <Input
-                id="name-1"
-                name="NomeCliente"
+                defaultValue={"nome do cliente"}
+                {...register("nome")}
+                id="nome"
                 placeholder="Digite o nome do cliente"
               />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="DescriçãoCliente">Descrição</Label>
+              <p>{errors.descrição?.message}</p>
               <Input
+                defaultValue={"descrição do cliente"}
                 id="descrição"
-                name="descrição"
                 placeholder="Escreva uma breve descrição"
+                {...register("descrição")}
               />
             </div>
 
@@ -78,22 +115,29 @@ export function DialogDemo() {
               <Label htmlFor="ModeloNegocioCliente">
                 Modelo de negocio do cliente
               </Label>
+
+              <p>{errors.modeloDeNegocio?.message}</p>
               <Input
+                defaultValue={"modelo de negocio do cliente"}
                 id="ModeloNegocioCliente"
-                name="ModeloNegocioCliente"
                 placeholder="Qual o modelo de negocio do seu cliente?"
+                {...register("modeloDeNegocio")}
               />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="contratoCliente">
                 Valor do contrato do cliente
               </Label>
+
+              <p>{errors.valorDoContrato?.message}</p>
               <Input
-                required
+                defaultValue={10000}
                 id="valorContrato"
-                name="valorContrato"
                 type="number"
                 placeholder="digite o valor por extenso: ex: 10000 "
+                {...register("valorDoContrato", {
+                  valueAsNumber: true,
+                })}
               />
             </div>
 
@@ -101,7 +145,10 @@ export function DialogDemo() {
               <Label htmlFor="DataInicioCliente">
                 Data de inicio do contrato
               </Label>
+
               <DayPicker
+                {...register("inicioDoContrato")}
+                id="DataInicioCliente"
                 required
                 mode="single"
                 selected={selected}
@@ -114,6 +161,7 @@ export function DialogDemo() {
               />
             </div>
           </div>
+
           <DialogFooter className={styles.Footer}>
             <DialogClose asChild>
               <Button variant="outline" className={styles.create}>
