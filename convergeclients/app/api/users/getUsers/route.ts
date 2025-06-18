938b42c8296cbcad7ff9be0032abf1e3f7ed8e3b
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-18 17:25:15 -0300

    Update route.ts

--- convergeclients/app/api/users/getUsers/route.ts
+++ convergeclients/app/api/users/getUsers/route.ts
@@ -4,8 +4,5 @@ const prisma = new PrismaClient();
 
 export async function GET() {
   const allUsers = await prisma.users.findMany();
-  if (allUsers.length > 0) {
-    return Response.json(allUsers, { status: 200 });
-  }
-  return Response.json({ message: "usuarios não encontrado" }, { status: 404 });
+  return Response.json(allUsers, { status: 200 });
 }
