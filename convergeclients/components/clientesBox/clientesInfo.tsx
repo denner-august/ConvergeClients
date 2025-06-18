1dce90fcfbed0065c4b24e104aa7343be93888e5
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2025-06-18 17:25:34 -0300

    Update clientesInfo.tsx

--- convergeclients/components/clientesBox/clientesInfo.tsx
+++ convergeclients/components/clientesBox/clientesInfo.tsx
@@ -14,7 +14,9 @@ interface clientesEncontradosProps {
 
 interface ClientesInfoProps {
   id: number;
+
   titulo: string;
+  ClientNome: string;
   descrição: string;
   dataInicio: string;
   modeloDeNegocio: string;
@@ -24,43 +26,51 @@ interface ClientesInfoProps {
 export function ClientesInfo(clientesEncontrados: clientesEncontradosProps) {
   const clientes = clientesEncontrados.clientes || [];
 
+  //  <h4>nome: {cliente.ClientNome}</h4>  preciso adicionar o nome do cliente amanhã
+
   return (
     <div>
       {clientes.length > 0 ? (
-        clientes.map((cliente: ClientesInfoProps) => (
-          <div key={cliente.id} className={styles.Cliente}>
-            <div className={styles.Contrato}>
-              <h3>{cliente.titulo}</h3>
-              <p>
-                Valor do Contrato:{" "}
-                {Number(cliente.valorAtualDoContrato).toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
-              </p>
-            </div>
+        clientes
+          .map((cliente: ClientesInfoProps) => (
+            <div key={cliente.id} className={styles.Cliente}>
+              <div className={styles.Contrato}>
+                <h3> {cliente.titulo}</h3>
 
-            <div className={styles.Contrato}>
-              <p>{cliente.descrição}</p>
-              <p>
-                Data de Início:
-                {cliente.dataInicio
-                  ? new Date(cliente.dataInicio).toLocaleDateString("pt-BR", {
-                      day: "2-digit",
-                      month: "2-digit",
-                      year: "numeric",
-                    })
-                  : "Data não informada"}
-              </p>
-            </div>
+                <p>
+                  Valor do Contrato:{" "}
+                  {Number(cliente.valorAtualDoContrato).toLocaleString(
+                    "pt-BR",
+                    {
+                      style: "currency",
+                      currency: "BRL",
+                    }
+                  )}
+                </p>
+              </div>
 
-            <div className={styles.Contrato}>
-              <p>Modelo de negocio: {cliente.modeloDeNegocio}</p>
+              <div className={styles.Contrato}>
+                <p>{cliente.descrição}</p>
+                <p>
+                  Data de Início:
+                  {cliente.dataInicio
+                    ? new Date(cliente.dataInicio).toLocaleDateString("pt-BR", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                      })
+                    : "Data não informada"}
+                </p>
+              </div>
+
+              <div className={styles.Contrato}>
+                <p>Modelo de negocio: {cliente.modeloDeNegocio}</p>
+              </div>
             </div>
-          </div>
-        ))
+          ))
+          .reverse()
       ) : (
-        <p>Nenhum cliente com esse nome encontrado</p>
+        <p>Nenhum cliente foi encontrado</p>
       )}
     </div>
   );
